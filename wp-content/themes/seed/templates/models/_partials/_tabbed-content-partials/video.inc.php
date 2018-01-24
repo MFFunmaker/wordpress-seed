cec6c56bbded033a1680ea925ae82e570fd4cc6d
Ike Borup <ike@visitpluto.com>
2018-01-24 04:42:43 -0700

    :fire: Remove unnecessary themes and MVC partials

--- wp-content/themes/seed/templates/models/_partials/_tabbed-content-partials/video.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$user = wp_get_current_user();
-if ( ( get_sub_field( 'member_only_section' ) && ( in_array( 'member', (array) $user->roles ) || in_array( 'business_member', (array) $user->roles ) ) ) || !get_sub_field( 'member_only_section' ) ) {
-	$model['tabSection'][$i]['tabContent'][$j]['layoutType'] = $contentRowLayout;
-	$model['tabSection'][$i]['tabContent'][$j]['videoType'] = get_sub_field( 'video_type' );
-	$model['tabSection'][$i]['tabContent'][$j]['videoCode'] = get_sub_field( 'video_code' );
-}
-?>
